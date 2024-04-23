161e0223c881c744e1f752c2d7df42b8394438dc
Akshat Mittal <akshatmittal2506@gmail.com>
2024-04-23 19:16:52 +0530

    initial websockets server setup

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import ws, { WebSocketServer } from "ws";
+
+const app = express();
+const port = 8080;
+
+const server = app.listen(port, () =>
+	console.info(`Server is listening at http://localhost:${port}`)
+);
+
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", (ws) => {
+	ws.on("message", (data) => {
+		console.log(`data from client: ${data}`);
+		ws.send("Gotcha!");
+	});
+});
